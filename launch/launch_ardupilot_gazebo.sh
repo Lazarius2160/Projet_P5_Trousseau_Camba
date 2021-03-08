38958577c9a0e2959a65e9ae9ddc65b17d200e5c
Lazarius2160 <76939787+Lazarius2160@users.noreply.github.com>
2021-03-08 18:11:47 +0100

    Don't need anylore

--- launch/launch_ardupilot_gazebo.sh
+++
@@ -1,8 +0,0 @@
-#This file will  launch a simulation with ArduPIlot SITL and Gazebo :
-
-#!/bin/bash
-
-cd ~/Pkg_Projet/ardupilot_ws/ardupilot/ArduCopter
-
-sim_vehicle.py -f gazebo-iris --console --map &
-gazebo --verbose worlds/iris_arducopter_runway.world &
